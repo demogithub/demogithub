19b0e74e8f624fc0f6de120388aa60c9d697d589
demogithub <pedro.jancachagua@gmail.com>
2012-01-20 00:04:29 -0500

    mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demogithub::Application.config.secret_token = '4ba202c94753d98029046b3769d534fa43fd02ec6685781c1f87cbfc026f5872cc354e0248eb3a547b33a5137c623ae63d03c5959d5caa7b4a332d1b8371a3d1'
